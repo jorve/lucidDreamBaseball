e89705a49fa84fa962fc571ab1489462abf003a3
Jeff Jorve <jjorve@gmail.com>
2021-01-20 18:46:39 -0500

    add first pick odds to lottery
    added the odds of getting the first overall pick to the draft results

--- lottery/ldb_lottery.py
+++ lottery/ldb_lottery.py
@@ -8,24 +8,27 @@ def gen_team_dict(team, entries):
     }
     return team_dict
 
-def gen_draft_dict(team, pick, odds):
+def gen_draft_dict(team, pick, odds, first_pick):
     draft_dict = {
         'team': team,
         'pick': pick,
-        'odds': odds
+        'odds': odds,
+        'first_pick': first_pick
     }
     return draft_dict
 
 lottery = []
 draft_order = []
 results = []
+total_entries = 0
 
-with open('teams_and_entries.csv') as f:
+with open('./lottery/teams_and_entries.csv') as f:
     reader = csv.reader(f)
     data = list(reader)
 
 for a in data:
     team = gen_team_dict(a[0], int(a[1]))
+    total_entries += int(a[1])
     lottery.append(team)
 
 while (len(draft_order) < len(lottery)):
@@ -42,8 +45,14 @@ while (len(draft_order) < len(lottery)):
     for team in lottery:
         if team['team'] is selected_team:
             team_odds = team['entries']/entries
-    selected_team_dict = gen_draft_dict(selected_team, len(draft_order), team_odds)
+            first_pick_odds = team['entries']/total_entries
+    selected_team_dict = gen_draft_dict(selected_team, len(draft_order), team_odds, first_pick_odds)
     results.append(selected_team_dict)
     pass
 
-print(results)
+keys = results[0].keys()
+
+with open('./lottery/ldb_draft_order.csv', 'wt')  as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(results)
